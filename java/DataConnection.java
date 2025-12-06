a2212cad4862bf0c5ad89f67fa3bbace7f746823
Miarintsoa Christian <ainafinaritra17@gmail.com>
2025-12-06 12:02:48 +0300

    Update DataConnection.java

--- java/DataConnection.java
+++ java/DataConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DataConnection {
     
     public static Connection getDBConnection() throws SQLException {
-       
+
         String url = "jdbc:postgresql://localhost:5432/product_management_db";
         String user = "product_manager_user";
         String password = "123456";
